7623c703904818a4aec8b5a59cc28f145621f623
“Arman <2770826g@student.gla.ac.uk>
2024-01-26 12:52:47 +0000

    Chapter5 Commit

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -80,8 +80,8 @@ WSGI_APPLICATION = 'tango_with_django_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    'ENGINE': 'django.db.backends.sqlite3',
+    'NAME': os.path.join(BASE_DIR, 'db.sqlite3')
     }
 }
 
