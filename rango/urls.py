d824ff4fedc85fa92a07f5b47689bc5308c0e721
“Arman <2770826g@student.gla.ac.uk>
2024-01-29 13:47:30 +0000

    Task6 Commit

--- rango/urls.py
+++ rango/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('about/', views.about, name='about'),
     path('index/', views.index, name='index'),
+    path('category/<slug:category_name_slug>/',
+        views.show_category, name='show_category'),
 ]
