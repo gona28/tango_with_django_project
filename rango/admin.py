d4c0acbb953db5dc05fec345cc8d13bbe040be29
“Arman <2770826g@student.gla.ac.uk>
2024-02-05 21:51:33 +0000

    Chapter9 Commit

--- rango/admin.py
+++ rango/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from rango.models import Category, Page
+from rango.models import UserProfile
+
 
 
 
@@ -13,5 +15,6 @@ class PageAdmin(admin.ModelAdmin):
 
 admin.site.register(Page, PageAdmin)
 admin.site.register(Category, CategoryAdmin)
+admin.site.register(UserProfile)
 
 
